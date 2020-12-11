ad6fcb6e987c77544b2210d64a6f2d022d4aa0f9
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2020-12-11 20:26:01 +0100

    App Upload

---
+++ src/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+	appTitle: {
+		backgroundColor: '#1565c0',
+		width: '100%',
+		color: "white",
+		height: '6vh',
+		paddingLeft: "2%",
+		display: 'flex',
+		textAlign: "center",
+	},
+});
